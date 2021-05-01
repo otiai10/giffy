84b2e58220563527cca6d73ce7a66a29319d3b16
Hiromu OCHIAI <otiai10@gmail.com>
2021-05-01 11:08:01 +0900

    Fix test

--- main.go
+++ main.go
@@ -25,16 +25,14 @@ var (
 	o = &options{}
 )
 
-func init() {
+func main() {
+
 	flag.StringVar(&o.input, "i", "", "Input source images")
 	flag.StringVar(&o.output, "o", "animated.gif", "Output file name")
 	flag.IntVar(&loop, "loop", 0, "Loop count (0 == infinite)")
 	flag.IntVar(&delay, "delay", 1000, "Delay in milliseconds")
 	flag.BoolVar(&quiet, "quiet", false, "Do not output verbose log")
 	flag.Parse()
-}
-
-func main() {
 
 	if err := run(o); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
